4ae338baeb4207f6b361e7e4197b89c2be300cbf
anirdm <YOUR_EMAIL>
2023-10-15 00:27:37 +0300

    Modify tseam-account.js

--- 03-Arrays-MORE-EXERCISE/05-tseam-account.js
+++ 03-Arrays-MORE-EXERCISE/05-tseam-account.js
@@ -38,15 +38,3 @@ function tseamAccount(arr) {
     }
     console.log(games.join(' '));  
 }
-
-tseamAccount(['CS WoW Diablo',
-
-'Install LoL',
-
-'Uninstall WoW',
-
-'Update Diablo',
-
-'Expansion CS-Go',
-
-'Play!']);
